180cafac673e8e76bc8cf5b4d32233229031c668
Abhishek Wagh <abhimwagh20@gmail.com>
2023-12-13 23:49:49 -0600

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation, createBrowserRouter } from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
+import CartMenu from "./scenes/global/CartMenu";
+import Footer from "./scenes/global/Footer";
+import Search from "./scenes/global/Search";
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Layout />,
+//     children: [
+//       {path: '/search', element: <search />}
+//     ]
+//   }
+// ])
+
+function App() {
+  return  <div className="app">
+      <BrowserRouter>
+      
+      <Navbar />
+      
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+          <Route path= '/search' element={<Search />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+  </div>;
+}
+
+export default App;
