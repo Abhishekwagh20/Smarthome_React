47d5d74febff0f4e5656dad6750ae58bb53f13e8
Abhishek Wagh <abhimwagh20@gmail.com>
2023-12-13 23:50:25 -0600

    Create productreviews.js

---
+++ client/src/Assi5/productreviews.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    date: { type: String, required: true }, 
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    
+    reviews: [reviewSchema],
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
